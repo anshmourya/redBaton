58c37605cac5c7c5051ca2d2d474bb1fd48859fb
ansh mourya <anshmourya657@gmail.com>
2024-01-31 11:38:49 +0530

    init: react-query , tailwind , shadcn

---
+++ src/hooks/useScrap.tsx
@@ -0,0 +1,16 @@
+// import React from "react";
+
+// const useScrap = () => {
+//   const scrapData = () => {
+//       try {
+
+//     } catch (error) {
+//       console.error(error);
+//       throw new Error("can't scrap the data something went wrong");
+//     }
+//   };
+
+//   return { scrapData };
+// };
+
+// export default useScrap;
